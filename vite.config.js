94c7108a0605e60f4ff10fa916b2de78679b98db
Darlington's Store <darlington@lexgo.com>
2026-04-07 13:06:34 +0000

    Configure GitHub Pages deployment via Actions

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/LetxGo/",
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
